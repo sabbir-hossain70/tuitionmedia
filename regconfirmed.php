23ccc69ccef644f483477bf9f1f8d0d28ada9949
sabbir-hossain70 <71shaikat@gmail.com>
2022-07-12 11:26:09 +0600

    data of teacher registration form taken in mysql teacher table

--- regconfirmed.php
+++ regconfirmed.php
@@ -1,12 +1,37 @@
 <?php
-$name=$_POST['name'];
-$email= $_POST['email'];
-$phone=$_POST['phone'];
+$teachername=$_POST['teachername'];
+$email= $_POST['email'];      
+$phone=$_POST['phone'];         
+$subjects=$_POST['subjects']; 
+$classes=$_POST['classes'];    
+$servername = "localhost";    
+$username = "root";
+$password = "";
+$dbname = "tuition_media";
 
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "INSERT INTO teachers (tname,email,phone,subjects,classes)
+VALUES ('$teachername','$email','$phone','$subjects','$classes');";
+
+
+$check=mysqli_query($conn,$sql);
+
+if ($check) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+$conn->close();
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +41,7 @@ $phone=$_POST['phone'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title></head>
     <body>
-    Welcome <?php echo $_POST["name"]; ?><br>
+    Welcome <?php echo $_POST["teachername"]; ?><br>
     Your email address is: <?php echo $_POST["email"]; ?><br>
     Your phone no is: <?php echo $_POST["phone"]; ?><br>
 
